08790ebea5fa1d658ecb4d746213dc7401a79e5d
Dennis Taylor <dennis.taylor@clio.com>
2021-08-12 19:24:38 -0700

    Add first VM test.

--- vm_test.go
+++ vm_test.go
@@ -1,2 +1,14 @@
 package main
 
+import (
+	"strings"
+)
+
+func ExampleVirtualMachine_basic_addition() {
+	vm := NewVirtualMachine()
+	compiler := NewCompiler(vm)
+	compiler.LoadCode(strings.NewReader(": foo ( -- n ) 1 2 + ; foo ."))
+	vm.Run()
+
+	// Output: 3
+}
